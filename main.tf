c201fbc910c4595ea03b7df3bd7539d84815ef65
itspradeepsingh <89738682+itspradeepsingh@users.noreply.github.com>
2023-05-06 06:30:10 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+# The configuration for the `remote` backend.
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
